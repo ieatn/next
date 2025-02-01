88b363f51d469f35324bfb8d934bb078a257119c
ieatn <ieatnubcake@gmail.com>
2025-01-31 17:45:04 -0800

    dynamic routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
     <main>
       <h1>the main page</h1>
       <Link href="/posts">go to posts</Link>
+      <Link href="/posts/first-post">go to first post</Link>
+      <Link href="/posts/second-post">go to second post</Link>
 
     </main>
   );
